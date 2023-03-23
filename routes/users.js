0631e8fabeb8649aac2979ecc2ec62c41fd2f5da
LiuGao <bgmmd922@gmail.com>
2023-03-23 13:16:44 +0800

    删除node_modules文件

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,45 @@ router.post("/loginpwd", (req, res, next) => {
         return;
       }
       if(result.length>0){
-           
+           //用户存在，判断密码是否正确
+           pool.getConnection((err,connection)=>{
+            if(err){
+              res.status(500).send('db连接错误')
+              console.log(err);
+              return
+            }
+            connection.query(sqlPwd,[userPwd],(err,result)=>{
+              if(err){
+                console.log('db connect err'+err);
+                res.status(500).send('数据库查询错误')
+                return
+              }
+              connection.release()
+              if(result.length>0){
+                //密码存在
+                res.status(200).send({
+                  code:200,
+                  msg:'登陆成功',
+                  success:true,
+                  data:result[0]
+                })
+              }
+              else{
+                //密码错误
+                res.status(401).send({
+                  code:401,
+                  msg:'密码错误'
+                })
+              }
+            })
+           })
+      }
+      else{
+        //用户不存在】
+        res.status(404).send({
+          code:404,
+          msg:'用户不存在'
+        })
       }
     });
   })
